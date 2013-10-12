7eb77c4c94be58740c406f451ce7996a8eb7b4e5
Nik Daftary <nikdaftary@gmail.com>
2013-10-12 16:06:17 -0500

    added forms for creating new products. Cannot render description and price for some reason

--- src/views/product-input-view.js
+++ src/views/product-input-view.js
@@ -11,25 +11,22 @@
       e.preventDefault();
 
       //Grab the new product's name, description & price
- 
+      var newProductName        = $(this.el).find('[name  = p_name]').val();
+      var newProductDescription = $(this.el).find('[desc  = p_desc]').val();
+      var newProductPrice       = $(this.el).find('[price = p_price]').val();
       
-      var newProductDescription = $(this.el).find('[desc=p_desc]').val();
-      var newProductPrice       = $(this.el).find('[price=p_price]').val();
-      var newProductName        = $(this.el).find('[name=p_name]').val();
-
-
-
 
       //Add new product to product collection
       this.collection.add({
-        
+
+        name: newProductName,
         desc: newProductDescription,
-        price: newProductPrice,
-        name: newProductName
-
-
-
+        price: newProductPrice
+        
       });
+
+
+      //output values to make sure we're retrieving user-inputted values
       console.log("product name: " + newProductName);
       console.log("description: " + newProductDescription);
       console.log("price: " + newProductPrice);
